047f234c1f413ec51ce86f596f9f4c60ec5db832
Grigory87 <dyakonoff.grigory@yandex.ru>
2023-04-01 16:00:39 +0300

    changed method search, add method for get specification

--- impl/src/main/java/ru/skillbox/diplom/group35/microservice/account/impl/service/AccountService.java
+++ impl/src/main/java/ru/skillbox/diplom/group35/microservice/account/impl/service/AccountService.java
@@ -4,16 +4,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-import ru.skillbox.diplom.group35.microservice.account.domain.model.Account;
 import ru.skillbox.diplom.group35.microservice.account.api.dto.AccountDto;
 import ru.skillbox.diplom.group35.microservice.account.api.dto.AccountSearchDto;
+import ru.skillbox.diplom.group35.microservice.account.domain.model.Account;
+import ru.skillbox.diplom.group35.microservice.account.domain.model.Account_;
 import ru.skillbox.diplom.group35.microservice.account.impl.mapper.AccountMapper;
 import ru.skillbox.diplom.group35.microservice.account.impl.repository.AccountRepository;
 
-
 import javax.transaction.Transactional;
+import java.time.ZonedDateTime;
 import java.util.UUID;
+
+import static ru.skillbox.diplom.group35.library.core.utils.SpecificationUtil.*;
 
 /**
  * service
@@ -35,7 +39,8 @@ public class AccountService {
     }
 
     public Page<AccountDto> search(AccountSearchDto searchDto, Pageable pageable) {
-        return null;
+        Page<Account> accounts = accountRepository.findAll(getSpecByAllFields(searchDto), pageable);
+        return accounts.map(accountMapper::mapToDto);
     }
 
     public AccountDto getById(UUID id) {
@@ -62,4 +67,17 @@ public class AccountService {
         });
     }
 
+    public static Specification<Account> getSpecByAllFields(AccountSearchDto searchDto) {
+        return getBaseSpecification(searchDto)
+                .and(in(Account_.id, searchDto.getIds(), true))
+                .and(notIn(Account_.id, searchDto.getBlockedByIds(), true))
+                .and(equal(Account_.firstName, searchDto.getFirstName(), true))
+                .and(equal(Account_.lastName, searchDto.getLastName(), true))
+                .and(likeLowerCase(Account_.firstName, searchDto.getFirstName(), true))
+                .and(likeLowerCase(Account_.lastName, searchDto.getLastName(), true))
+                .and(between(Account_.birthDate,
+                        searchDto.getAgeTo() == null ? null : ZonedDateTime.now().minusYears(searchDto.getAgeTo()),
+                        searchDto.getAgeFrom() == null ? null : ZonedDateTime.now().minusYears(searchDto.getAgeFrom()),
+                        true));
+    }
 }
