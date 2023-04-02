0b5510b2dc318ba6bd0ea54ca8146bdc2c652825
Georgii Vinogradov <dev626@yandex.ru>
2023-04-02 18:51:10 +0300

    add security

--- api/src/main/java/ru/skillbox/diplom/group35/microservice/account/api/client/AccountFeignClient.java
+++ api/src/main/java/ru/skillbox/diplom/group35/microservice/account/api/client/AccountFeignClient.java
@@ -1,24 +1,20 @@
-package ru.skillbox.diplom.group35.microservice.account.api.resource;
+package ru.skillbox.diplom.group35.microservice.account.api.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.skillbox.diplom.group35.library.core.controller.BaseController;
 import ru.skillbox.diplom.group35.microservice.account.api.dto.AccountDto;
 import ru.skillbox.diplom.group35.microservice.account.api.dto.AccountSearchDto;
 
 import java.util.UUID;
 
-/**
- * AccountController
- *
- * @author Georgii Vinogradov
- */
-
-
-@RequestMapping("/api/v1/account")
-public interface AccountController extends BaseController<AccountDto, AccountSearchDto> {
+@FeignClient(
+        name = "microservice-account",
+//        url = "http://localhost:8080",
+        path = "/api/v1/account")
+public interface AccountFeignClient{
 
     @GetMapping("/statistic")
     ResponseEntity<Integer> getAccountCount();
@@ -26,32 +22,21 @@ public interface AccountController extends BaseController<AccountDto, AccountSea
     @GetMapping("/me")
     ResponseEntity<AccountDto> get(@RequestHeader("Authorization") String bearerToken);
 
-    @Override
     @GetMapping("/{id}")
     ResponseEntity<AccountDto> getById(@PathVariable(name = "id") UUID id);
 
-    @GetMapping("/search")
-    ResponseEntity<Page<AccountDto>> search(AccountSearchDto searchDto, Pageable pageable);
-
-    @Override
-    @GetMapping
-    ResponseEntity<Page<AccountDto>> getAll(AccountSearchDto searchDto, Pageable pageable);
-
-    @Override
     @PostMapping
     ResponseEntity<AccountDto> create(@RequestBody AccountDto dto);
 
     @PutMapping(value = "/me")
     ResponseEntity<AccountDto> update(@RequestHeader("Authorization") String bearerToken, @RequestBody AccountDto dto);
 
-    @Override
     @PutMapping
     ResponseEntity<AccountDto> update(@RequestBody AccountDto dto);
 
     @DeleteMapping(value = "/me")
     void delete(@RequestHeader("Authorization") String bearerToken);
 
-    @Override
     @DeleteMapping(value = "/{id}")
     void deleteById(@PathVariable(name = "id") UUID id);
 }
--- impl/src/main/java/ru/skillbox/diplom/group35/microservice/account/Application.java
+++ impl/src/main/java/ru/skillbox/diplom/group35/microservice/account/Application.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import ru.skillbox.diplom.group35.library.core.annotation.EnableBaseRepository;
+import ru.skillbox.diplom.group35.library.core.annotation.EnableSecurity;
+import ru.skillbox.diplom.group35.microservice.account.api.client.AccountFeignClient;
 
 /**
  * Application
@@ -12,11 +15,13 @@ import ru.skillbox.diplom.group35.library.core.annotation.EnableBaseRepository;
  * @author Denis_Kholmogorov
  */
 
+
+@EnableSecurity
 @EnableBaseRepository
-@SpringBootApplication(
-        exclude = {SecurityAutoConfiguration.class,
-        ManagementWebSecurityAutoConfiguration.class}
-)
+@EnableFeignClients(basePackages = "ru.skillbox.diplom.group35.microservice.account.api.client")
+@SpringBootApplication
+//        (exclude = {SecurityAutoConfiguration.class,
+//        ManagementWebSecurityAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
