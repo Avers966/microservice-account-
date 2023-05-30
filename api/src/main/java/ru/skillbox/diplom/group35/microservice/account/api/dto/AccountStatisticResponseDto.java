6b434ee4b646dc9311973633b153672eb7325aa5
Grigory87 <dyakonoff.grigory@yandex.ru>
2023-05-30 14:33:43 +0300

    fix statistic

--- api/src/main/java/ru/skillbox/diplom/group35/microservice/account/api/dto/AccountStatisticResponseDto.java
+++ api/src/main/java/ru/skillbox/diplom/group35/microservice/account/api/dto/AccountStatisticResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.skillbox.diplom.group35.library.core.dto.statistic.StatisticPerDateDto;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 /**
@@ -19,4 +20,10 @@ import java.util.List;
 public class AccountStatisticResponseDto extends StatisticPerDateDto {
     private List<AccountCountPerAge> countPerAge;
     private List<StatisticPerDateDto> countPerMonth;
+
+    public AccountStatisticResponseDto(ZonedDateTime date, Integer count, List<AccountCountPerAge> countPerAge, List<StatisticPerDateDto> countPerMonth) {
+        super(date, count);
+        this.countPerAge = countPerAge;
+        this.countPerMonth = countPerMonth;
+    }
 }
